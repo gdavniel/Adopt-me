a378929849be6ad6cee9fafa7c0ec8cd4c695dd9
Daniel <gdel.daniel@gmail.com>
2019-10-16 20:22:41 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import {render} from 'react-dom'
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", { id: "id" }, [
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, { name: "carlos ", animal: "dog" }),
-    React.createElement(Pet, { name: "luna", animal: "dog" }),
-    React.createElement(Pet, { name: "mia", animal: "cat" })
-  ]);
+//   return React.createElement("div", { id: "id" }, [
+//     React.createElement("h1", {}, "Adopt me"),
+//     React.createElement(Pet, { name: "carlos ", animal: "dog" }),
+//     React.createElement(Pet, { name: "luna", animal: "dog" }),
+//     React.createElement(Pet, { name: "mia", animal: "cat" })
+//   ]);
+
+// parentesis en return => decirle al return que leta todo como una linea 
+
+    return (
+        <div>
+            <h1>Adopt me</h1>
+            <Pet name={'carlos'} animal={'cat'}/>
+            <Pet name={'juan'} animal={'dog'}/>
+            <Pet name={'luna'} animal={'cat'}/>
+        </div>
+    )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const Pet = ({ name, animal }) => {
-    return React.createElement("div", { id: "id" }, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal)
-    ]);
+    // return React.createElement("div", { id: "id" }, [
+    //     React.createElement("h1", {}, name),
+    //     React.createElement("h2", {}, animal)
+    // ]);
+
+    // se pone un div como envoltorio, porque el return solo puede devolver 1 cosa, no 2 resltados
+    // se puede usar fragments
+    
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{animal}</h2>
+        </div>
+    )
 };
 
 export default Pet;
